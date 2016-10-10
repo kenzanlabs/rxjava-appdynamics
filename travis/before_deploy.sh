5fca3812dfd836b6268ba3d16596fb443249b4f7
mattcarrier <mcarrieruri@gmail.com>
2016-10-10 17:50:38 -0400

    Add encrypted variables

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_db86af8b5f2d_key -iv $encrypted_db86af8b5f2d_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_34b5aec44fbc_key -iv $encrypted_34b5aec44fbc_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import travis/codesigning.asc
 fi
